64c740042da77cc04ca4776fb9bd07c4c2604b85
takashimayuusuke <taka.z34.vq37@gmail.com>
2020-12-21 15:15:07 +0900

    アプリケーションコントローラにparameter実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_contoroller?
+
+  privata
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name_chinese_character, :first_name_chinese_character, :last_name_kana, :first_name_kana, :birth])
+  end
 end
