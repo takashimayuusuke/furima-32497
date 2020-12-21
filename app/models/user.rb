1bd10ca7feada32e321bbe51ff3ae3a0d2faad51
takashimayuusuke <taka.z34.vq37@gmail.com>
2020-12-21 16:43:18 +0900

    userモデル、itemモデルとのアソシエーションとバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has
+
+
+  validates :name, presence: true
+  validates :last_name_chinese_character, presence: true
+  validates :first_name_chinese_character, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth, presence: true
+
 end
