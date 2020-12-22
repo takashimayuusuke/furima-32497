c8f8e0bd5e39e216b15d1ae7c03aa80b25f6851e
takashimayuusuke <taka.z34.vq37@gmail.com>
2020-12-22 15:33:54 +0900

    userモデル、with_optionsで重複箇所をまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,13 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/
   validates :password, format:{ with: VALID_PASSWORD_REGEX }
-
-  validates :name, presence: true
-  validates :last_name_chinese_character, presence: true, format: {with: /\A[一-龥ぁ-ん]/ }
-  validates :first_name_chinese_character, presence: true, format: {with: /\A[一-龥ぁ-ん]/ }
-  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth, presence: true
+  with_options presence: true do
+    validates :name
+    validates :last_name_chinese_character, format: {with: /\A[一-龥ぁ-ん]/ }
+    validates :first_name_chinese_character, format: {with: /\A[一-龥ぁ-ん]/ }
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth
+  end
 
 end
