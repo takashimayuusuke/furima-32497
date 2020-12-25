75e2649b5fefbed25d3246b4616c109969482688
takashimayuusuke <taka.z34.vq37@gmail.com>
2020-12-25 21:02:28 +0900

    integer型のシングルクオーテーション削除

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :item do
     name                          { 'アイテム' }
-    explanation_category_id       { '2' }
-    explanation_condition_id      { '2' }
-    delivery_fee_id               { '2' }
-    shipment_source_address_id    { '2' }
-    shipment_day_id               { '2' }
+    explanation_category_id       { 2 }
+    explanation_condition_id      { 2 }
+    delivery_fee_id               { 2 }
+    shipment_source_address_id    { 2 }
+    shipment_day_id               { 2 }
     explanation                   { '説明文' }
     price                         { '1000' }
     association :user
