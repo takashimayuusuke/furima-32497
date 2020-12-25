b8237ea9026e960341040c2be213d00934b20470
takashimayuusuke <taka.z34.vq37@gmail.com>
2020-12-25 14:47:41 +0900

    itemモデルのFactoryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name                          {"アイテム"}
+    explanation_category_id       {"2"}
+    explanation_condition_id      {"2"}
+    delivery_fee_id               {"2"}
+    shipment_source_address_id    {"2"}
+    shipment_day_id               {"2"}
+    explanation                   {"説明文"}
+    price                         {"1000"}
+    image                         {Faker::Lorem.sentence}
+    association :user
   end
 end
