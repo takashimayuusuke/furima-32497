706f47225a2fafc8cce5500242c32d9f3bc13fac
takashimayuusuke <taka.z34.vq37@gmail.com>
2020-12-25 19:39:10 +0900

    itemモデル、numericality: { other_than: 1 }をwith_optionsメソッドでまとめる

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,11 +16,12 @@ class Item < ApplicationRecord
     validates_inclusion_of :price, in: 300..9_999_999
   end
 
-  validates :explanation_category_id, numericality: { other_than: 1 }
-  validates :explanation_condition_id, numericality: { other_than: 1 }
-  validates :delivery_fee_id, numericality: { other_than: 1 }
-  validates :shipment_source_address_id, numericality: { other_than: 1 }
-  validates :shipment_day_id, numericality: { other_than: 1 }
-
+  with_options numericality: { other_than: 1 } do
+    validates :explanation_category_id
+    validates :explanation_condition_id
+    validates :delivery_fee_id
+    validates :shipment_source_address_id
+    validates :shipment_day_id
+  end
   validates :image, presence: true
 end
