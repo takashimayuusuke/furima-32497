43240f213e5fbeb5ab4cbf9c4e590131849b4a97
takashimayuusuke <taka.z34.vq37@gmail.com>
2021-01-04 15:17:54 +0900

    buysテーブルと、shippingsテーブルにそれぞれhぞんするための処理を記述

--- app/forms/buy_shipping.rb
+++ app/forms/buy_shipping.rb
@@ -1,4 +1,4 @@
-class Buy_Shipping
+class BuyShipping
 
   include ActiveModel::Model
   attr_accessor :user, :item, :postal_code, :shipment_source_address_id,
@@ -6,9 +6,15 @@ class Buy_Shipping
   :building, :phone, :buy,
 
   with_options presence: true do
-    validates :postal_code
-    validates :shipment_source_address_id
+    validates :postal_code, format: {with:  /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :shipment_source_address_id, numericality: {other_than: 0, message: "Shipment source address is not a number"}
     validates :municipality
     validates :address
-    validates :phone
+    validates :phone, format: {with: /\A\d{11}\z/}
   end
+
+  def save
+    Buy.create(user: user, item: item)
+    Shipping.create(postal_code: postal_code,shipment_source_address_id: shipment_source_address_id, municipality: municipality, address: address, building: building, phone: phone, buy: buy)
+  end
+end
