31e22b365e438862868d56c1b0fa1289528fb39c
takashimayuusuke <taka.z34.vq37@gmail.com>
2021-01-04 14:38:16 +0900

    shippingモデルのバリデーションを、フォームオブジェクトへ移動

--- app/forms/buy_shipping.rb
+++ app/forms/buy_shipping.rb
@@ -1,3 +1,14 @@
 class Buy_Shipping
 
   include ActiveModel::Model
+  attr_accessor :user, :item, :postal_code, :shipment_source_address_id,
+  :municipality, :address,
+  :building, :phone, :buy,
+
+  with_options presence: true do
+    validates :postal_code
+    validates :shipment_source_address_id
+    validates :municipality
+    validates :address
+    validates :phone
+  end
--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,11 +1,4 @@
 class Shipping < ApplicationRecord
   belongs_to :buy
 
-  with_options presence: true do
-    validates :postal_code
-    validates :shipment_source_address_id
-    validates :municipality
-    validates :address
-    validates :phone
-  end
 end
