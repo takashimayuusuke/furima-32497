f72cd0cb62b2a4b8359332ff6e71c89f32e818d7
takashimayuusuke <taka.z34.vq37@gmail.com>
2021-01-04 11:20:24 +0900

    shippingsテーブル、マイグレーションファイル作成

--- db/migrate/20210104003230_create_shippings.rb
+++ db/migrate/20210104003230_create_shippings.rb
@@ -1,6 +1,13 @@
 class CreateShippings < ActiveRecord::Migration[6.0]
   def change
     create_table :shippings do |t|
+      t.string :postal_code,                  null: false
+      t.integer :shipment_source_address_id,  null: false
+      t.string :municipality,                 null: false
+      t.string :address,                      null: false
+      t.string :building
+      t.string :phone,                        null: false
+      t.references :buy,                      foreign_key: true
 
       t.timestamps
     end
