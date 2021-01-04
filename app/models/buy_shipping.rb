d32523ba11ae4da7c5f97f5e3c90fdae2ef27194
takashimayuusuke <taka.z34.vq37@gmail.com>
2021-01-04 18:10:11 +0900

    createアクションへのルートパスを指定

--- app/models/buy_shipping.rb
+++ app/models/buy_shipping.rb
@@ -3,19 +3,21 @@ class BuyShipping
   include ActiveModel::Model
   attr_accessor :user, :item, :postal_code, :shipment_source_address_id,
   :municipality, :address,
-  :building, :phone, :buy,
+  :building, :phone, :buy
 
   with_options presence: true do
-    validates :postal_code, format: {with:  /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :shipment_source_address_id, numericality: {other_than: 0, message: "Shipment source address is not a number"}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :shipment_source_address_id, numericality: { other_than: 1, message: "Shipment source address is not a number" }
     validates :municipality
     validates :address
-    validates :phone, format: {with: /\A\d{11}\z/}
+    validates :phone, format: { with: /\A\d{11}\z/ }
   end
+  
 
   def save
-    user = User.create(name: name,
-    Buy.create(user_id: user_id, item: item)
+    user = User.create(name: name )
+    item = Item.create(item: item )
+    Buy.create(user_id: user.id, item: item.id)
     Shipping.create(postal_code: postal_code,shipment_source_address_id: shipment_source_address_id, municipality: municipality, address: address, building: building, phone: phone, buy: buy)
   end
 end
