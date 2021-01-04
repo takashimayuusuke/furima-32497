05ced591f9ab50a700e592c4da28890aeaab97c4
takashimayuusuke <taka.z34.vq37@gmail.com>
2021-01-04 21:42:05 +0900

    formオブジェクトのBuyShippingクラスについてのテストコードのファイル作成

---
+++ spec/factories/buy_shippings.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :buy_shipping do
+    postal_code { '123-4567'} 
+    shipment_source_address_id { 2 }
+    municipality { '取手市' }
+    address { '上町1-1-1' }
+    building { 'マンション1012' }
+    phone { '09012345678' }
+  end
+end
