42506a3ae0a67abbdf1a318df82ae8e9bf3531ac
takashimayuusuke <taka.z34.vq37@gmail.com>
2021-01-04 18:20:03 +0900

    createアクションの処理とprivateメソッドにparamsを記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,5 +3,19 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @buy_shipping = BuyShipping.new
   end
+  
+  def create
+    @buy_shipping =BuyShipping.new(buy_shipping_params)
+    if @buy_shipping.valid?
+      @buy_shipping.save
+      redirect_to root_path(@buy_shipping)
+    else
+      render action: :index
+  end
 end
 
+private
+
+def buy_shipping_params
+  params.require(:buy_shipping).permit(:user, :item, :postal_code, :shipment_source_address_id,:municipality, :address, :building, :phone, :buy)
+end
