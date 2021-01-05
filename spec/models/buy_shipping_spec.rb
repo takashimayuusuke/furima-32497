3c8c28b9278eb9b72b20412e263f0a582d85efc7
takashimayuusuke <taka.z34.vq37@gmail.com>
2021-01-05 09:56:28 +0900

    buy_shipping.rbファイル、テストコードの記述の実装

--- spec/models/buy_shipping_spec.rb
+++ spec/models/buy_shipping_spec.rb
@@ -9,28 +9,53 @@ RSpec.describe BuyShipping, type: :model do
     it '郵便番号、発送先、市区町村、番地、建物名、電話番号が正しく入力されていれば保存できる'do
       expect(@buy_shipping).to be_valid
     end
+
     it 'postal_codeが空では保存できない' do
-    
+      @buy_shipping.postal_code = ''
+      @buy_shipping.valid?
+      expect(@buy_shipping.errors.full_messages).to include("Postal code can't be blank")
     end
+
     it 'postal_codeにハイフンがないと保存できない' do
-    
+      @buy_shipping.postal_code = '1234567'
+      @buy_shipping.valid?
+      expect(@buy_shipping.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
     end
+
     it 'shipment_source_address_idが空では登録できない' do
-    
+      @buy_shipping.shipment_source_address_id = ''
+      @buy_shipping.valid?
+      expect(@buy_shipping.errors.full_messages).to include("Shipment source address can't be blank")
     end
+
     it 'shipment_source_address_idが1では登録できない' do
-    
+      @buy_shipping.shipment_source_address_id = 1
+      @buy_shipping.valid?
+      expect(@buy_shipping.errors.full_messages).to include("Shipment source address Shipment source address is not a number")
     end
+
     it 'municipalityが空では登録できない' do
-    
+      @buy_shipping.municipality = ''
+      @buy_shipping.valid?
+      expect(@buy_shipping.errors.full_messages).to include("Municipality can't be blank")
     end
+    
     it 'addressが空では登録できない' do
-    
+      @buy_shipping.address = ''
+      @buy_shipping.valid?
+      expect(@buy_shipping.errors.full_messages).to include("Address can't be blank")
     end
+    
     it 'phoneが空では登録できない' do
-    
+      @buy_shipping.phone = ''
+      @buy_shipping.valid?
+      expect(@buy_shipping.errors.full_messages).to include("Phone can't be blank")
     end
+    
     it 'phoneにハイフンが含まれていると登録できない' do
+      @buy_shipping.phone = '090-1234-5678'
+      @buy_shipping.valid?
+      expect(@buy_shipping.errors.full_messages).to include("Phone is invalid")
     end
   end
 end
